c7e74e6dfe37266487bb6d515062c080648f5fb4
Gustavo Oliveira <gusta.coliveira07@gmail.com>
2020-10-25 12:36:35 -0300

    Capturando tamanho
    Capturando o tamanho da tela para que possa ser limitado o local onde os mosquitos estão nascendo.

---
+++ game.js
@@ -0,0 +1,14 @@
+//alert('Teste');
+
+//Capiturando o tamanho da tela 
+let altura = 0;
+let largura = 0;
+
+function ajustaTamanhoTela(){
+
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+    console.log(altura, largura);
+    
+}
+
